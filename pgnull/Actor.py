9aef37042ef4dbec5c8c59e7c613c453b2a26593
Sinthoras39 <digc0820@gmail.com>
2025-01-18 16:57:00 +0100

    scale

--- pgnull/Actor.py
+++ pgnull/Actor.py
@@ -8,10 +8,11 @@ class Actor(Sprite):
         super(Actor, self).__init__()
         self.pygame_obj = self
         path = "images/" + name + ".png"
-        self.image = pygame.image.load(path)
+        self.original_image = pygame.image.load(path)
+        self.image = self.original_image
         self.rect = self.image.get_rect()
-
-        self.colliderect = self.rect.colliderect
+        self.scale(1, 1)
+        self.rotate(45)
 
     @property
     def pos(self):
@@ -34,6 +35,21 @@ class Actor(Sprite):
     @pos.setter
     def pos(self, value):
         self.rect.x, self.rect.y = value
+
+    def scale(self, x, y):
+        self.scale_val = (x,y)
+        original_width, original_height = self.original_image.get_size()
+
+        new_width = int(original_width * x)
+        new_height = int(original_height * y)
+
+        self.image = pygame.transform.scale(self.image, (new_width, new_height))
+        self.rect = self.image.get_rect(center=self.rect.center)
+        self.colliderect = self.rect.colliderect
+
+    def rotate(self, angle):
+        self.image = pygame.transform.rotate(self.original_image, angle)
+        self.scale(*self.scale_val)
 
     def draw(self):
         glob_singleton["game"].screen.blit(self.image, self.rect)
--- pgnull/Screen.py
+++ pgnull/Screen.py
@@ -2,6 +2,8 @@ from pygame import display
 import pygame.font
 from pgnull.utils import Colors
 
+from pygame import draw
+
 class Screen:
     def __init__(self, WIDTH, HEIGHT, caption):
         self.pygame_obj = display.set_mode((WIDTH, HEIGHT))
@@ -10,13 +12,15 @@ class Screen:
         self.fill = self.pygame_obj.fill
         self.blit = self.pygame_obj.blit
 
+        self.draw = draw
+
     def draw_text(self, text , topleft, font=None, fontsize = 32, color=Colors.BLACK, font_kwargs={}, render_kwargs={}):
         font = pygame.font.Font(font, fontsize, **font_kwargs)
         text_obj = font.render(text, True, color, **render_kwargs)
         self.blit(text_obj, topleft)
 
     def draw_rect(self, rect, color):
-        pygame.draw.rect(self.pygame_obj, color, rect)
+        draw.rect(self.pygame_obj, color, rect)
 
     def get_text_size(self, text, font=None, fontsize=32, font_kwargs={}):
         font = pygame.font.Font(font, fontsize, **font_kwargs)
