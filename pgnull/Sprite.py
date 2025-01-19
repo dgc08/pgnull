f3bd693c770bb3fa2077adacac6fd77143a0b97a
Sinthoras39 <digc0820@gmail.com>
2025-01-19 14:55:36 +0100

    better scale

--- pgnull/Sprite.py
+++ pgnull/Sprite.py
@@ -1,19 +1,25 @@
 import pygame.image
 from pygame.sprite import Sprite as pygame_sprite
+from pygame.math import *
 
 from .utils import glob_singleton
 
 class Sprite(pygame_sprite):
-    def __init__(self, name, image_path=None, scale=(1,1), angle=0):
+    def __init__(self, image_path, pos=None, scale=(1,1), angle=0):
         super(Sprite, self).__init__()
         self.pygame_obj = self
-        if not image_path:
-            image_path = "images/" + name + ".png"
         self.original_image = pygame.image.load(image_path)
         self.image = self.original_image
         self.rect = self.image.get_rect()
-        self.scale(*scale)
-        self.rotate(angle)
+
+        self.__scale_val = Vector2(scale)
+        self.__angle = angle
+        self.scale = scale
+        self.rotation = angle
+
+        if pos:
+            self.pos = pos
+            print("set to ", pos)
 
     @property
     def pos(self):
@@ -37,20 +43,41 @@ class Sprite(pygame_sprite):
     def pos(self, value):
         self.rect.x, self.rect.y = value
 
-    def scale(self, x, y):
-        self.scale_val = (x,y)
+    def __rescale_and_rotate(self):
+        #prev_img = self.image
+
         original_width, original_height = self.original_image.get_size()
 
-        new_width = int(original_width * x)
-        new_height = int(original_height * y)
+        new_width = int(original_width * self.__scale_val[0])
+        new_height = int(original_height * self.__scale_val[1])
 
-        self.image = pygame.transform.scale(self.image, (new_width, new_height))
+        self.image = pygame.transform.rotate(
+            pygame.transform.scale(self.original_image, (new_width, new_height)),
+            self.__angle
+        )
         self.rect = self.image.get_rect(center=self.rect.center)
+
+        #self.image = pygame.transform.scale(self.image, (new_width, new_height))
+        #self.rect = self.image.get_rect(center=self.rect.center)
         self.colliderect = self.rect.colliderect
 
-    def rotate(self, angle):
-        self.image = pygame.transform.rotate(self.original_image, angle)
-        self.scale(*self.scale_val)
+    @property
+    def scale(self):
+        return self.__scale_val
+    @scale.setter
+    def scale(self, value):
+        self.__scale_val = Vector2(value)
+        self.__rescale_and_rotate()
+
+    @property
+    def rotation(self):
+        return self.__angle
+    @rotation.setter
+    def rotation(self, value):
+        value = value % 360
+        self.__angle = value
+        self.__rescale_and_rotate()
+
 
     def draw(self):
         glob_singleton["game"].screen.blit(self.image, self.rect)
